20a4cc4604512c5966afa9320b5900a15a6293b3
victorzx9 <victordsf2008@gmail.com>
2025-08-22 22:20:47 -0300

    aula 03

---
+++ aula03/app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
+export default function RootLayout() {
+  return <GluestackUIProvider mode="light"><Stack /></GluestackUIProvider>;
+}
